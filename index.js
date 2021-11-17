bb5369b0c869cf4d548f17cd1b3debc752f5f7bb
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-11-16 21:47:50 -0300

    estruturacao mvc

--- index.js
+++ index.js
@@ -5,35 +5,7 @@ const port = 8080;
 app.use(express.json());
 
 app.get("/", function(req,res) {
-    res.send("Minha primeira requisição")
-});
-
-app.get("/segunda-req", function(req,res) {
-    res.send("Minha segunda requisição")
-});
-
-app.get("/com-parametros", function(req,res) {
-    if (req.query.nome === "Beatriz") {
-        res.send("Beatriz chamou requisição")
-    }
-
-    res.send("Com parametros funciona! Sabadou " + req.query.nome + " " + req.query.sobrenome);
-});
-
-app.post("/meu-primeiro-post", (req, res) => {
-    console.log(req.body);
-    res.send("Meu primeiro post")
-});
-
-app.put("/meu-primeiro-put/:id", (req, res) => {
-    console.log(req.body, req.params.id);
-
-    res.send("Meu put funciona");
-});
-
-app.delete("/meu-primeiro-delete/:id", (req, res) => {
-    console.log(req.params.id);
-    res.send("Meu delete funciona " + req.params.id);
+    res.send("Dasa Educa - Artigos");
 });
 
 app.listen(port, function() {
